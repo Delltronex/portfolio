b8896ced914174373ddb4faf37d3f816fd667e16
Kaushal Shukla <103252985+Delltronex@users.noreply.github.com>
2025-11-09 22:35:53 +0530

    Update Experience.jsx

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -6,9 +6,9 @@ export default function Experience() {
       <h1>EXPERIENCE</h1>
       <div className="timeline">
         <div className="timeline-item">
-          <h2>Web Dev Intern - REnot Solutions</h2>
-          <p>Worked on influencer portal using HTML, CSS, and Java. Major role in frontend.</p>
-          <span>2021</span>
+          <h2>Web Dev Intern - Reknot Solutions Pvt. Ltd.</h2>
+          <p>Designed and developed an Influencer Portal to connect influencers with brands.</p>
+          <span>2022</span>
         </div>
       </div>
     </section>
