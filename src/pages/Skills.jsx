995f45a1d93d240118f38147d7a26f3c24f8bd90
Kaushal Shukla <103252985+Delltronex@users.noreply.github.com>
2025-11-11 01:43:59 +0530

    Update Skills.jsx

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -5,11 +5,11 @@ export default function Skills() {
     <section className="page-section">
       <h1>SKILLS</h1>
       <div className="grid">
-        <div className="card"><h3>React / Next.js</h3><p>Modern frontend frameworks</p></div>
-        <div className="card"><h3>Node.js / Express</h3><p>Backend development</p></div>
-        <div className="card"><h3>AI & ML</h3><p>Model integration, LangChain, LLaMA</p></div>
-        <div className="card"><h3>Blockchain / Web3</h3><p>Smart contracts, DApp architecture</p></div>
-        <div className="card"><h3>Python / Flask</h3><p>AI automation, data processing</p></div>
+        <div className="card">Languages</h3><p>Python, Javascript</p></div>
+        <div className="card"><h3>Frameworks</h3><p>Tensorflow, langchain, Flask, Qerw.AI, N8N, Huggingface</p></div>
+        <div className="card"><h3>Technologis</h3><p>LLM, RAG, AI/ML, VecorDB, Promt Engineering</p></div>
+        <div className="card"><h3>Development Tools</h3><p>VS Code, AWS, Docker, Git, Github</p></div>
+        <div className="card"><h3>Database</h3><p>MySQL, PostgreSQl</p></div>
       </div>
     </section>
   );
